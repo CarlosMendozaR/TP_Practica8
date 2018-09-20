6542269bfac0b7c4086b55e5708e70aec6e56813
ferna <ferna@LAPTOP-PNQUFFH0>
2018-09-19 22:45:35 -0500

    Agregue los c2 hasta c4 puse limpieza de pantallas y eliminar porfavor chequen que corra bien

--- Nueva solución/Colecciones P8/Program.cs
+++ Nueva solución/Colecciones P8/Program.cs
@@ -15,9 +15,51 @@ namespace Colecciones_P8
             c1.LeerDatos();
             c1.GenerarCurp();
             Console.WriteLine(c1);
+            Console.ReadKey();
+            Console.Clear();
+            
+            c2.LeerDatos();
+            c2.GenerarCurp();
+            Console.WriteLine(c2);
+            Console.ReadKey();
+            Console.Clear();
+
+            c3.LeerDatos();
+            c3.GenerarCurp();
+            Console.WriteLine(c3);
+            Console.ReadKey();
+            Console.Clear();
+
+
+            c4.LeerDatos();
+            c4.GenerarCurp();
+            Console.WriteLine(c4);
+            Console.ReadKey();
+            Console.Clear();
+
 
             ArrayList Curps = new ArrayList();
             Curps.Add(c1);
+            Curps.Add(c2);
+            Curps.Add(c3);
+            Curps.Add(c4);
+
+
+            Console.WriteLine("La lista de CURPS es la siguiente");
+            for (int i = 0; i < 4; i++)
+            {
+                string cur = Curps[i].ToString();
+                Console.WriteLine("{0}.- \t {1}",i,cur);
+            }
+
+            Console.WriteLine("Desea eliminar alguno \n 1) Si \n 2)No");
+            int des=int.Parse(Console.ReadLine());
+            if (des==1)
+            {
+                Console.WriteLine("Qué curp desea borrar");
+                int borrar = int.Parse(Console.ReadLine());
+                Curps.Remove(borrar);
+            }
 
 
             Console.ReadLine();
