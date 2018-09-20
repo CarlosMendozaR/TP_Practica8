6f0ee825be1a7a0bbfc5c31c17e6fb696e896f79
Gerardo Chapa <gerardochapa1000@hotmail.com>
2018-09-19 21:48:39 -0500

    aiuda

---
+++ Colección/Curp.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+public class Curp
+{
+    class Curp
+    {
+        public string CURP;
+
+        private string nombre;
+        private string apellidoPat;
+        private string apellidoMat;
+        private string año;
+        private string mes;
+        private string dia;
+        private string ciudad;
+        private char sexo;
+
+        public Curp()
+        {
+
+        }
+
+        public void GenerarCurp()
+        {
+            CURP = apellidoPat[0].ToString();
+            foreach (char letra in apellidoPat)
+            {
+                if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U')
+                {
+                    CURP = CURP + letra;
+                    break;
+                }
+            }
+            CURP = CURP + apellidoMat[0] + nombre[0] + año + mes + dia + sexo + ciudad;
+
+            foreach (char letra in apellidoPat)
+            {
+                if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U')
+                {
+                }
+                else if (letra == apellidoPat[0])
+                {
+
+                }
+                else
+                {
+                    CURP = CURP + letra;
+                    break;
+                }
+            }
+
+            foreach (char letra in apellidoMat)
+            {
+                if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U')
+                {
+                }
+                else if (letra == apellidoMat[0])
+                {
+
+                }
+                else
+                {
+                    CURP = CURP + letra;
+                    break;
+                }
+            }
+
+            foreach (char letra in nombre)
+            {
+                if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U')
+                {
+                }
+                else if (letra == nombre[0])
+                {
+
+                }
+                else
+                {
+                    CURP = CURP + letra;
+                    break;
+                }
+            }
+        }
+
+        public void LeerDatos()
+        {
+            Console.WriteLine("ESCRIBA LOS SIGUIENTES DATOS EN MAYUSCULA:\nEscriba su NOMBRE");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escriba su APELLIDO PATERNO");
+            apellidoPat = Console.ReadLine();
+            Console.WriteLine("Escriba su APELLIDO MATERNO");
+            apellidoMat = Console.ReadLine();
+            Console.WriteLine("Escriba su año de nacimineto a dos digitos");
+            año = Console.ReadLine();
+            Console.WriteLine("Escriba su mes de nacimiento a dos digitos");
+            mes = Console.ReadLine();
+            Console.WriteLine("Escriba su dia de nacimineto a dos digitos");
+            dia = Console.ReadLine();
+            Console.WriteLine("Escriba las primeras dos letras de su ciudad natal");
+            ciudad = Console.ReadLine();
+            Console.WriteLine("Escriba la letra de su sexo H-Hombre o M-Mujer");
+            sexo = char.Parse(Console.ReadLine());
+        }
+
+        public override string ToString()
+        {
+            return CURP;
+        }
+    }
+}
--- Colección/Program.cs
+++ Colección/Program.cs
@@ -5,26 +5,22 @@ namespace Colección
 {
     class Program
     {
-        static void Main()
-        {
-            ArrayList persona = new ArrayList();
-            Console.WriteLine("Para crear un nuevo CURP, introduzca los datos que se le solicitan en mayúscula.\nPRIMER NOMBRE:");
-            persona.Add(Console.ReadLine());
-            Console.WriteLine("APELLIDO:");
-            persona.Add(Console.ReadLine());
-            Console.WriteLine("AÑO DE NACIMIENTO:");
-            persona.Add(Console.ReadLine());
-            Console.WriteLine("MES:");
-            persona.Add(Console.ReadLine());
-            Console.WriteLine("DÍA:");
-            persona.Add(Console.ReadLine());
-            Console.WriteLine("ESTADO (Primeras dos letras):");
-            persona.Add(Console.ReadLine());
-            Console.WriteLine("SEXO (H ó M):");
-            persona.Add(Console.ReadLine());
+       static void Main()
+       {
+            Curp c1 = new Curp();
+            Curp c2 = new Curp();
+            Curp c3 = new Curp();
+            Curp c4 = new Curp();
+
+            c1.LeerDatos();
+            c1.GenerarCurp();
+            Console.WriteLine(c1);
+
+            ArrayList Curps = new ArrayList();
+            Curps.Add(c1);
 
 
-
-        }
+            Console.ReadLine();
+       }
     }
 }
